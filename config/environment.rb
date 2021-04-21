364ca8e118f833d88fca41bd1b2075b398451db3
andrewjh24 <haywardandrew@outlook.com>
2021-04-21 15:36:55 +0100

    Stying Validation and Flash Messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Get rid of field with error that ruby creates when error message pops up
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance |
+  html_tag.html_safe
+end
