bbf3068d9c94ef08e0bdabe799a96766112a88b0
andrewjh24 <haywardandrew@outlook.com>
2021-04-24 14:16:17 +0100

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Congratulations #{@user.username} on joining the HomeFitness family!!! You have successfully created and account"
+      redirect_to workouts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
