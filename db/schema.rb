ad9484915e5aea0430a07f327e2cad4526032b21
andrewjh24 <haywardandrew@outlook.com>
2021-04-26 19:43:05 +0100

    Add Admin column within user database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_173002) do
+ActiveRecord::Schema.define(version: 2021_04_26_183532) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_04_23_173002) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "workouts", force: :cascade do |t|
