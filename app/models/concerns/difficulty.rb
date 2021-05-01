106c2228b6fa6d92bdf3f18d68e2f55eea770128
andrewjh24 <haywardandrew@outlook.com>
2021-05-01 19:22:38 +0100

    Validation Testing

--- app/models/concerns/difficulty.rb
+++ app/models/concerns/difficulty.rb
@@ -1,3 +1,4 @@
 class Difficulty < ApplicationRecord
-
+  validates :name, presence: true, length: { minimum: 4, maximum: 20}
+  validates_uniqueness_of :name
 end
--- test/models/difficulty_test.rb
+++ test/models/difficulty_test.rb
@@ -2,9 +2,34 @@ require 'test_helper'
 
 class DifficultyTest < ActiveSupport::TestCase
 
-  test "difficulty should be valid" do
+  def setup
     @difficulty = Difficulty.new(name: "Beginner")
+  end
+
+
+  test "difficulty should be valid" do
     assert @difficulty.valid?
+  end
+
+  test "difficulty should be present" do
+    @difficulty.name = " "
+    assert_not @difficulty.valid?
+  end
+
+  test "difficulty is not unique" do
+    @difficulty.save
+    @difficulty2 = Difficulty.new(name: "Beginner")
+    assert_not @difficulty2.valid?
+  end
+
+  test "difficulty name is too long" do
+    @difficulty.name = "d" * 21
+    assert_not @difficulty.valid?
+  end
+
+  test "difficulty name is too short" do
+    @difficulty.name = "aa"
+    assert_not @difficulty.valid?
   end
 
 end
