301b29750d67499e4bb1ce07dd328f0614b58684
andrewjh24 <haywardandrew@outlook.com>
2021-05-05 20:04:26 +0100

    Seed File populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: John, email: "john@hotmail.com",password: "password",admin: false)
+Workout.create(title: 'Chest Workout',description: '20 press ups',user_id:User.last)
